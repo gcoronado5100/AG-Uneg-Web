145fb12b0266648508fba723c5dfccbe430379b6
Caolina <rcaolina15@gmail.com>
2022-08-29 23:41:51 -0400

    migraciones agregadas

---
+++ database/migrations/2022_08_30_012417_create_puntos_table.php.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('puntos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->string('acuerdo_instrucciones');
+            $table->unsignedBigInteger('agenda_id');
+            $table->foreign('agenda_id')->references('id')->on('agendas');
+            $table->unsignedBigInteger('estado_id');
+            $table->foreign('estado_id')->references('id')->on('estados');
+            $table->date('ultima_actualizacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
